b31b1bd5e9930aca53ba60893e58df9f716e6feb
annalynn02 <145460339+annalynn02@users.noreply.github.com>
2025-06-27 14:37:28 +0200

    nog weer anders

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,2 @@
 // JavaScript Document
 console.log("hi");
-
-function verhoogAantal(type) {
-    const el = document.getElementById(`aantal-${type}`);
-    let aantal = parseInt(el.textContent);
-    el.textContent = aantal + 1;
-}
